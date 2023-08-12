12a398450f8cc3b31bcc1bdc67f902944e5ceaa2
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-08-12 19:52:47 +0600

    updeth this row colmun

--- src/components/ProducList.vue
+++ src/components/ProducList.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="row row-cols-1 row-cols-md-3 card-group-55">
-    <div
-      class="card h-100 p-4"
-      style="width: 100%"
-      v-for="item in products"
-      :key="item.id"
-    >
-      <img :src="item.imgUrl" class="card-img-top" alt="item.title" />
-      <div class="card-body">
-        <p class="card-title">{{ item.title }}</p>
-        <p class="card-text">{{ item.price }}</p>
-        <a href="#" @click="addToCart(item)" class="btn btn-primary"
-          >Add to cart</a
-        >
-        <products />
+  <div class="contener">
+    <div class="row row-cols-1 row-cols-md-3 card-group-55">
+      <div class="card h-100 w-100 p-4" v-for="item in products" :key="item.id">
+        <img :src="item.imgUrl" class="card-img-top" alt="item.title" />
+        <div class="card-body">
+          <p class="card-title">{{ item.title }}</p>
+          <p class="card-text">{{ item.price }}</p>
+          <a href="#" @click="addToCart(item)" class="btn btn-primary"
+            >Add to cart</a
+          >
+          <products />
+        </div>
       </div>
     </div>
   </div>
@@ -39,5 +36,9 @@ export default {
 <style scoped>
 .card {
   border: none;
+}
+
+.row {
+  column-gap: px;
 }
 </style>
